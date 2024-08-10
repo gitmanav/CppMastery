ee9d3a2dcc4a408a9c7725cbb8ece690e9bc17e8
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-08-10 19:23:45 +0530

    Time: 92 ms (19.61%), Space: 12.9 MB (72.52%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    if (i!=j){
+                        return {i,j};
+                    }
+                }
+            }
+        }
+        return{};
+        
+    }
+};
