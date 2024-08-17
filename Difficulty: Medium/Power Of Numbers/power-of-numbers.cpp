46f70e75a598568d622498a3b5aa1786dd015eac
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-08-17 07:57:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Power Of Numbers/power-of-numbers.cpp
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.cpp
@@ -12,28 +12,28 @@ class Solution{
     {
        //Your code here
        if(R==0)
+       {
+           return 1;
+       }
+       if(R==1)
+       {
+           return N;
+       }
+       
+       long long ans=power(N,R/2)%1000000007; 
+        ans=(ans*ans)%1000000007;
+        if(R%2==0)
         {
-            return 1;
+            return ans%1000000007;
         }
-        if(R==1)
+        else
         {
-            return N;
+            return (N*ans)%1000000007;
         }
         
-       long long ans=power(N,R/2)%1000000007;
-       ans=(ans*ans)%1000000007;
-       
-       if(R%2==0)
-       {
-           return ans%1000000007;
-       }
-       else
-        return N*ans%1000000007;
-    
     }
 
 };
-
 
 //{ Driver Code Starts.
 
