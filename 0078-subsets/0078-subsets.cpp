1fc104b23cf44d9728d5b1a301a4a543861cb67e
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-08-24 08:23:31 +0530

    Time: 3 ms (46.89%), Space: 15.4 MB (7.85%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    void solve(vector<int> nums,int index, vector<int>output,vector<vector<int>>&ans)
+    void solve(vector<int> nums,int index,vector<vector<int>>& ans,vector<int> temp)
     {
-        //Base Case
         if(index>=nums.size())
         {
-            ans.push_back(output);
+            ans.push_back(temp);
             return;
         }
         //exclude
-        solve(nums,index+1,output,ans);
-
-        //Include
-        int element=nums[index];
-        // cout<<element<<" ";
-        output.push_back(element);
-        solve(nums,index+1,output,ans);
+        solve(nums,index+1,ans,temp);
+        int ele=nums[index];
+        temp.push_back(ele);
+        solve(nums,index+1,ans,temp);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> output;
+        vector<int> temp;
         int index=0;
-        solve(nums,index,output,ans);
+        solve(nums,index,ans,temp);
         return ans;
     }
 };
