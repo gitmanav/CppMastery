ee716aff27d32a7b0dc843456c8c694defda6928
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-09-01 22:25:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -17,24 +17,20 @@ void printArray(int arr[], int size)
 
 // } Driver Code Ends
 class Solution
-{
+{ 
     public:
     void merge(int arr[], int l, int m, int r)
     {
          // Your code here
-        //  int mid=l+(r-l)/2;
          int len1=m-l+1;
          int len2=r-m;
-         
          int *first=new int[len1];
          int *second=new int[len2];
-         
          int k=l;
          for(int i=0;i<len1;i++)
          {
              first[i]=arr[k++];
          }
-         
          k=m+1;
          for(int i=0;i<len2;i++)
          {
@@ -44,7 +40,6 @@ class Solution
          int ind1=0;
          int ind2=0;
          k=l;
-         
          while(ind1<len1 && ind2<len2)
          {
              if(first[ind1]<second[ind2])
@@ -74,11 +69,7 @@ class Solution
             return;
         }
         int mid=l+(r-l)/2;
-        
-        //left part solve kro
         mergeSort(arr,l,mid);
-        
-        //right part solve kro
         mergeSort(arr,mid+1,r);
         merge(arr,l,mid,r);
     }
