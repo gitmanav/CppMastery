afcb932631986440da214d24b0293040cbe20bc2
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-09-14 08:59:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.cpp
+++ Difficulty: Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.cpp
@@ -3,52 +3,58 @@
 
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
-class Solution{
-public:
 
-	void rearrange(int arr[], int n) {
-	    // code here
-	    int *first=new int[n];
-	    int *second=new int[n];
-	    int j=0,h=0,pos=0,neg=0;
-	    for(int i=0;i<n;i++)
-	    {
-	       if(arr[i]>=0){
-	            first[j++]=arr[i];
-	            pos++;
-	       }
-	       else{
-	            second[h++]=arr[i];
-	            neg++;
-	       }
-	    }
-	    j=0,h=0;
-	    for(int i=0;i<n;i++)
-	    {
-	        if(i%2==0 && pos!=0)
-	        {
-	            arr[i]=first[j++];
-	            pos--;
-	        }
-	        else if(i%2!=0 && neg!=0)
-	        {
-	            arr[i]=second[h++];
-	            neg--;
-	        }
-	        else if(neg!=0)
-	        {
-	            arr[i]=second[h++];
-	            neg--;
-	        }
-	        else
-	        {
-	            arr[i]=first[j++];
-	            pos--;
-	        }
-	    }
-	}
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    void rearrange(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        vector<int> first(n);
+        vector<int> second(n);
+        int j=0,k=0;
+        int pos=0,neg=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>=0)
+            {
+                first[j++]=arr[i];
+                pos++;
+            }
+            else
+            {
+                second[k++]=arr[i];
+                neg++;
+            }
+        }
+        j=0;
+        k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0 && pos!=0)
+            {
+                arr[i]=first[j++];
+                pos--;
+            }
+            else if(i%2!=0 && neg!=0)
+            {
+                arr[i]=second[k++];
+                neg--;
+            }
+            else if(neg!=0)
+            {
+                arr[i]=second[k++];
+                neg--;
+            }
+            else
+            {
+                arr[i]=first[j++];
+                pos--;
+            }
+        }
+    }
 };
 
 //{ Driver Code Starts.
@@ -56,16 +62,20 @@ public:
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n, i;
-        cin >> n;
-        int arr[n];
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
+        vector<int> arr;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
         }
         Solution ob;
-        ob.rearrange(arr, n);
-        for (i = 0; i < n; i++) {
+        ob.rearrange(arr);
+        for (int i = 0; i < arr.size(); i++) {
             cout << arr[i] << " ";
         }
         cout << "\n";
