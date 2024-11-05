d06250fea115ff8c36772ee0e6ca297a28df9278
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-11-05 16:20:51 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (38.89%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    //Approach 1
+    //Approach 2
+        //SC --> O(N)+O(M)
+        //TC --> O(N)   
         unordered_set<int> set_1(begin(nums1),end(nums1));
-        unordered_set<int> set_2;     
-        
+        vector<int> res;
         for(auto it:nums2)
         {
             if(set_1.find(it) != set_1.end())
             {
-                set_2.insert(it);
+                res.push_back(it);
+                set_1.erase(it);
             }
         }
-        vector<int> res(begin(set_2),end(set_2));
         return res;
     }
 };
