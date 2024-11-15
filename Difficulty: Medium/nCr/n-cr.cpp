f7dbb599fd844cce0a15396fd0c47ce5b22993e7
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-11-15 11:23:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        // code here
+        long long res=1;
+        
+        for(int i=0;i<r;i++)
+        {
+            res*=(n-i);
+            res/=(i+1);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, r;
+        cin >> n >> r;
+
+        Solution ob;
+        cout << ob.nCr(n, r) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
