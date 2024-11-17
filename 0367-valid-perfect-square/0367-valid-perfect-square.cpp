8807b113b3d8b7f9e73a8c71bc549bf1840c8163
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-11-17 18:49:18 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (50.60%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low=1;
+        int high=num;
+        
+        while(low<=high)
+        {
+            long long mid=low+(high-low)/2;
+            
+            if((mid*mid)==num)
+            {
+                return true;
+            }
+            else if(mid*mid>num)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+            
+        }
+        return false;
+    }
+};
