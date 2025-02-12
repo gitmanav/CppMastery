b49937a83f1b2f3f70a7941832da54559c7a82fb
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2025-02-12 22:19:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Wave Array/wave-array.cpp
+++ Difficulty: Easy/Wave Array/wave-array.cpp
@@ -1,50 +1,56 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 // #include <algorithm>
 
 
 // } Driver Code Ends
-class Solution{
-    public:
+class Solution {
+  public:
     // arr: input array
-    // n: size of array
-    //Function to sort the array into a wave-like array.
-    void convertToWave(int n, vector<int>& arr){
+    // Function to sort the array into a wave-like array.
+    void convertToWave(vector<int>& arr) {
+        int n =arr.size();
+        int temp;
         
-        // Your code here
-        int swap=n/2;
         for(int i=0;i<n-1;i+=2)
         {
-            int temp=arr[i];
+            temp=arr[i];
             arr[i]=arr[i+1];
             arr[i+1]=temp;
         }
-        
-        
+        // code here
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
-    int t,n;
-    cin>>t; //Input testcases
-    while(t--) //While testcases exist
-    {
-        cin>>n; //input size of array
-        vector<int> a(n); //declare vector of size n
-        for(int i=0;i<n;i++)
-            cin>>a[i]; //input elements of array
-        sort(a.begin(),a.end());
-        Solution ob;
-        ob.convertToWave(n, a);
+int main() {
 
-        for(int i=0;i<n;i++)
-            cout<<a[i]<<" "; //print array
-            
-        cout<<endl;
+    int t, n;
+    cin >> t; // Input testcases
+    cin.ignore();
+    while (t--) // While testcases exist
+    {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        sort(a.begin(), a.end());
+        Solution ob;
+        ob.convertToWave(a);
+
+        for (int i = 0; i < a.size(); i++)
+            cout << a[i] << " ";
+
+        cout << endl;
+        cout << "~" << endl;
     }
 }
 // } Driver Code Ends
