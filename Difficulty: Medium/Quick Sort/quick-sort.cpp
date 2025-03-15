ac777176df7c0486c8a4b75a269c517bf59aaae8
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2025-03-15 13:50:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -1,87 +1,81 @@
 //{ Driver Code Starts
-#include <stdio.h>
 #include <bits/stdc++.h>
 using namespace std;
 
 /* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        printf("%d ", num);
     printf("\n");
 }
 
+
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to sort an array using quick sort algorithm.
-    void quickSort(int arr[], int low, int high)
-    {
+
+class Solution {
+  public:
+    // Function to sort an array using quick sort algorithm.
+    void quickSort(vector<int>& arr, int low, int high) {
         // code here
-        if(low>=high)
+        if(low<high)
         {
-            return;
+            int q=partition(arr,low,high);
+            quickSort(arr,low,q-1);
+            quickSort(arr,q+1,high);
         }
-        int p=partition(arr,low,high);
-        quickSort(arr,low,p-1);
-        quickSort(arr,p+1,high);
     }
-    
-    public:
-    int partition (int arr[], int low, int high)
-    {
-       // Your code here
-       int cnt=0;
-       int pivot=arr[low];
-       
-       //count krenge kitne elements chote h pivot element se 
-       for(int i=low+1;i<=high;i++)
-       {
-           if(arr[i]<=pivot)
-           {
-               cnt++;
-           }
-       }
-       int pivotIndex=low+cnt;
-       swap(arr[pivotIndex],arr[low]);
-       //left and right vala part sambhal lete h 
-       int i=low;
-       int j=high;
-       while(i<pivotIndex && j>pivotIndex)
-       {
-           while(arr[i]<=pivot)
-           {
-               i++;
-           }
-           while(arr[j]>pivot)
-           {
-               j--;
-           }
-           if(i<pivotIndex && j>pivotIndex)
-           {
-               swap(arr[i++],arr[j--]);
-           }
-       }
-       return pivotIndex;
+
+  public:
+    // Function that takes last element as pivot, places the pivot element at
+    // its correct position in sorted array, and places all smaller elements
+    // to left of pivot and all greater elements to right of pivot.
+    int partition(vector<int>& arr, int low, int high) {
+        // code here
+        int x=arr[high];
+        int i=low-1;
+        
+        for(int j=low;j<high;j++)
+        {
+            if(arr[j]<=x)
+            {
+                i=i+1;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]=temp;
+        return i+1;
     }
 };
 
 
 //{ Driver Code Starts.
-int main()
-{
-    int arr[1000],n,T,i;
-    scanf("%d",&T);
-    while(T--){
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-      Solution ob;
-    ob.quickSort(arr, 0, n-1);
-    printArray(arr, n);
+
+int main() {
+
+    int T;
+    scanf("%d", &T);
+    getchar(); // to consume newline after T
+
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.quickSort(arr, 0, arr.size() - 1);
+        printArray(arr);
     }
+
     return 0;
 }
+
 // } Driver Code Ends
