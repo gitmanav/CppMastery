ac5c2641b1178236d924650217454ea7fbe13d0e
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2025-06-09 22:21:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Prime Number/prime-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool isPrime(int n) {
+        // code here
+        if(n<=1)
+        {
+            return false;
+        }
+        for(int i=2;i<n;i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
