5d59773f578f7522681f9e1202381032862cd073
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2025-06-22 14:41:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -1,17 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Function to print an array */
-void printArray(const vector<int>& arr) {
-    for (int num : arr)
-        printf("%d ", num);
-    printf("\n");
-}
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     // Function to sort an array using quick sort algorithm.
@@ -19,10 +5,11 @@ class Solution {
         // code here
         if(low<high)
         {
-            int q=partition(arr,low,high);
-            quickSort(arr,low,q-1);
-            quickSort(arr,q+1,high);
+            int p=partition(arr,low,high);
+            quickSort(arr,low,p-1);
+            quickSort(arr,p+1,high);
         }
+        
     }
 
   public:
@@ -30,52 +17,20 @@ class Solution {
     // its correct position in sorted array, and places all smaller elements
     // to left of pivot and all greater elements to right of pivot.
     int partition(vector<int>& arr, int low, int high) {
+        
         // code here
         int x=arr[high];
         int i=low-1;
-        
         for(int j=low;j<high;j++)
         {
             if(arr[j]<=x)
             {
                 i=i+1;
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+                swap(arr[i],arr[j]);
             }
         }
-        int temp=arr[i+1];
-        arr[i+1]=arr[high];
-        arr[high]=temp;
+        swap(arr[i+1],arr[high]);
         return i+1;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int T;
-    scanf("%d", &T);
-    getchar(); // to consume newline after T
-
-    while (T--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        ob.quickSort(arr, 0, arr.size() - 1);
-        printArray(arr);
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
